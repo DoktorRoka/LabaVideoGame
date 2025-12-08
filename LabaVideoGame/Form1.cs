222ae60bcba05e9406de912b35032cc8d18f6622
Roka <maximkalosha@gmail.com>
2025-12-08 19:18:29 +0300

    - Генерация звезд

--- LabaVideoGame/Form1.cs
+++ LabaVideoGame/Form1.cs
@@ -12,9 +12,52 @@ namespace LabaVideoGame
 {
     public partial class Form1 : Form
     {
+
+        private Random rnd;
+        private int seed; // сид для звезд, поведения тарелочницы и т.д
         public Form1()
         {
             InitializeComponent();
+
+            this.BackColor = Color.Black;
+            this.DoubleBuffered = true;
+
+            Random tmp = new Random();
+
+            seed = tmp.Next(1, 20);
+
+            rnd = new Random(seed);
+
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+                
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawStars(e.Graphics);
+        }
+
+        private void DrawStars(Graphics g)
+        {
+            int width = this.ClientSize.Width;
+            int height = this.ClientSize.Height;
+
+            int starsCount = 200; 
+
+            g.Clear(Color.Black);
+
+            for (int i = 0; i < starsCount; i++)
+            {
+                int x = rnd.Next(0, width);
+                int y = rnd.Next(0, height);
+
+                g.FillRectangle(Brushes.White, x, y, 2, 2);
+            }
+        }
+
     }
 }
